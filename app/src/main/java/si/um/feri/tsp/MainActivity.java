9c988ccb61332b4c14e69838ec2e24a4b74c0b88
LinuxAlex <alex.marinic22@gmail.com>
2023-01-08 10:01:53 +0100

    Added 2 resources files to project and the test code to main class

--- app/src/main/java/si/um/feri/tsp/MainActivity.java
+++ app/src/main/java/si/um/feri/tsp/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import si.um.feri.tsp.Utility.RandomUtils;
+import si.um.feri.tsp.problems.TSP;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RandomUtils.setSeedFromTime(); // nastavi novo seme ob vsakem zagonu main metode (vsak zagon bo drugačen)
+
+        // primer zagona za problem eil101.tsp
+        for (int i = 0; i < 100; i++) {
+            TSP eilTsp = new TSP(this.getResources().openRawResource(R.raw.bays29).toString(), 10000);
+            GA ga = new GA(100, 0.8, 0.1);
+            TSP.Tour bestPath = ga.execute(eilTsp);
+
+            // shrani min, avg in std
+        }
+        System.out.println(RandomUtils.getSeed()); // izpiše seme s katerim lahko ponovimo zagon
     }
 }
